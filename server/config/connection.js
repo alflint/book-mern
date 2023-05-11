25f8c6755c78fb9ddd7cb6729295bf57907220f9
alfint <alexandreaflint1111@gmail.com>
2023-05-11 07:19:19 -0600

    hard coded envs

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test');
+mongoose.connect('mongodb+srv://Cluster78125:mELgHhDKNQ94PAOT@cluster78125.ubza9vy.mongodb.net/?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -20,9 +20,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 async function startApolloServer() {
 
